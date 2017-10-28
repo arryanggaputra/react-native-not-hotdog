ab2152502f07437ccd15f5abc2e3449ed1ada061
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2017-10-28 16:20:41 +0700

    Add cameraButton component

---
+++ src/screens/CameraScreen/component/cameraButton.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import { View, Text, TouchableOpacity } from "react-native";
+import * as Animatable from "react-native-animatable";
+import Style from "../../../assets/style.js";
+
+class CameraButton extends Component {
+  render() {
+    return (
+      <Animatable.View
+        animation={this.props.animation}
+        duration={1000}
+        style={Style.floatingArea}
+      >
+        <TouchableOpacity
+          style={[Style.button, this.props.style, { width: 180 }]}
+          onPress={() => {
+            this.props.onPress();
+          }}
+        >
+          <Text style={[Style.buttonText, { color: "white" }]}>
+            {this.props.text}
+          </Text>
+        </TouchableOpacity>
+        {this.props.children}
+      </Animatable.View>
+    );
+  }
+}
+
+export default CameraButton;
