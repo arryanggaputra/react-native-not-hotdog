7422a63af2d9210c85cbf27908e5d9d52196c114
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2017-10-29 13:15:00 +0700

    Add react-native-sound to play music on app start

--- android/app/src/main/java/com/nothotdog/MainApplication.java
+++ android/app/src/main/java/com/nothotdog/MainApplication.java
@@ -3,6 +3,7 @@ package com.nothotdog;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.zmxv.RNSound.RNSoundPackage;
 import com.rnfs.RNFSPackage;
 import com.lwansbrough.RCTCamera.RCTCameraPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNSoundPackage(),
             new RNFSPackage(),
             new RCTCameraPackage()
       );
