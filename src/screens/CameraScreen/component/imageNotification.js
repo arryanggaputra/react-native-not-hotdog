303b3eb44d47ec8dbaa924c18585d6ec57b0194b
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2017-10-29 13:52:38 +0700

    Play chime sound if props.visible is true

--- src/screens/CameraScreen/component/imageNotification.js
+++ src/screens/CameraScreen/component/imageNotification.js
@@ -1,7 +1,30 @@
 import React, { Component } from "react";
 import { View, Image, StyleSheet, Dimensions } from "react-native";
+import Sound from "react-native-sound";
+
+const soundPath = "chime.mp3";
 
 class ImageNotification extends Component {
+  /**
+   * whenever props has changed
+   * we need to change the visibility property
+   *
+   * @param {props.visible} props
+   */
+  componentWillReceiveProps(props) {
+    // if image is showing, we play the sound
+    if (props.visible) {
+      this.sound = new Sound(soundPath, Sound.MAIN_BUNDLE, error => {
+        if (error) {
+          console.log("failed to load the sound", error);
+          return;
+        }
+        this.sound.setNumberOfLoops(0);
+        this.sound.play();
+      });
+    }
+  }
+
   render() {
     return (
       <View style={style.floatingViewTop}>
