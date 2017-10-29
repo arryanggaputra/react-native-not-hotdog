64d2acb86ea37d78a1b12b6b698b7f6394c5cbe0
Arryangga Aliev Pratamaputra <arryanggaputra@gmail.com>
2017-10-29 12:03:06 +0700

    Fix slpash screen page

--- src/screens/CameraScreen/index.js
+++ src/screens/CameraScreen/index.js
@@ -6,14 +6,17 @@ import {
   DeviceEventEmitter,
   StatusBar,
   TouchableOpacity,
+  ActivityIndicator,
   Image
 } from "react-native";
 import { NavigationActions } from "react-navigation";
 import * as Animatable from "react-native-animatable";
 import Camera from "react-native-camera";
 import ImageNotification from "./component/imageNotification";
+import EvaluateIndicator from "./component/evaluateIndicator";
 import CameraButton from "./component/cameraButton";
 import Style from "../../assets/style.js";
+import { ValidateTheHotdog, ConvertToBase64 } from "../../lib/ImageProcessing";
 
 const hotdogTrue = require("../../assets/hotdog_true.png");
 const hotdogFalse = require("../../assets/hotdog_false.png");
@@ -21,23 +24,27 @@ const hotdogFalse = require("../../assets/hotdog_false.png");
 class CameraScreen extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      showRetake: true,
-      showRetakeAnimation: "slideInUp",
-      showEvaluating: false,
-      showEvaluatingAnimation: "slideInUp",
-      showResult: false,
-      showResultAnimation: "slideInUp"
-    };
-  }
-
-  componentDidMount() {
     this.handleBackButton = this.handleBackButton.bind(this);
     this.takePicture = this.takePicture.bind(this);
     this.showEvaluating = this.showEvaluating.bind(this);
     this.showResult = this.showResult.bind(this);
     this.showRetake = this.showRetake.bind(this);
+    this.processImage = this.processImage.bind(this);
 
+    this.state = {
+      showRetake: true,
+      showRetakeAnimation: "slideInUp",
+      showEvaluating: false,
+      showResult: false,
+      showResultAnimation: "slideInUp",
+      isHotdog: false,
+      showImageNotification: false,
+      previewImage: false,
+      previewImageUri: null
+    };
+  }
+
+  componentDidMount() {
     BackHandler.addEventListener("hardwareBackPress", this.handleBackButton);
   }
 
@@ -52,48 +59,79 @@ class CameraScreen extends Component {
   }
 
   takePicture() {
-    this.showResult();
-    // const options = {};
-    // this.camera
-    //   .capture({ metadata: options })
-    //   .then(data => console.log(data))
-    //   .catch(err => console.error(err));
+    const options = {};
+    this.camera
+      .capture({ metadata: options })
+      .then(data => {
+        setTimeout(() => {
+          this.setState({
+            // showRetake: false,
+            previewImage: true,
+            previewImageUri: data.path
+          });
+          this.processImage(data);
+        }, 100);
+      })
+      .catch(err => console.error(err));
+  }
+
+  processImage(data) {
+    this.showEvaluating();
+    ConvertToBase64(data.path).done(res => {
+      ValidateTheHotdog(res).done(res => {
+        this.setState(
+          {
+            showEvaluating: false, //hide evaluating area
+            isHotdog: res,
+            showImageNotification: true // show image notification
+          },
+          () => this.showResult()
+        );
+      });
+    });
   }
 
   showRetake() {
     this.setState(
       {
         showRetake: true,
+        previewImage: false,
+        showImageNotification: false,
         showRetakeAnimation: "slideInUp",
         showResultAnimation: "zoomOut" //zoom out Result area
       },
       () => {
         setTimeout(() => {
           this.setState({
-            showResult: false, // hide result area
-            showEvaluating: false // hide evaluating area
+            showResult: false // hide result area
           });
         }, 1500);
       }
     );
   }
-  showEvaluating() {}
 
-  showResult() {
+  showEvaluating() {
     this.setState(
       {
-        showResult: true,
-        showResultAnimation: "slideInUp",
-        showRetakeAnimation: "zoomOut"
+        showEvaluating: true,
+        showRetakeAnimation: "zoomOut" //zoom out capture camera button
       },
       () => {
+        //   wait 1.5 second
         setTimeout(() => {
           this.setState({
-            showRetake: false
+            showRetake: false // hide capture camera button
           });
         }, 1500);
       }
     );
+  }
+
+  showResult() {
+    this.setState({
+      showResult: true,
+      showResultAnimation: "slideInUp"
+    });
   }
 
   render() {
@@ -101,15 +139,32 @@ class CameraScreen extends Component {
       <View style={[Style.container, { flexDirection: "row", flex: 1 }]}>
         <StatusBar hidden={true} />
 
-        <ImageNotification source={hotdogTrue} visible={false} />
+        <ImageNotification
+          source={this.state.isHotdog ? hotdogTrue : hotdogFalse}
+          visible={this.state.showImageNotification}
+        />
+
+        {this.state.showEvaluating && <EvaluateIndicator />}
 
         <Camera
           ref={cam => {
             this.camera = cam;
           }}
+          captureTarget={Camera.constants.CaptureTarget.disk}
+          captureQuality={Camera.constants.CaptureQuality.preview}
           style={Style.cameraPreview}
           aspect={Camera.constants.Aspect.fill}
         />
+
+        {this.state.previewImage && (
+          <Image
+            style={{ width: "100%", height: "100%" }}
+            source={{
+              uri: this.state.previewImageUri,
+              isStatic: true
+            }}
+          />
+        )}
 
         {this.state.showRetake && (
           <CameraButton
